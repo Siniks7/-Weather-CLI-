a145317695f600531c7c83495976957dad7b79fe
Nikolay Sponarovskiy <siniks7@yandex.ru>
2024-02-26 22:06:10 +0700

    api

--- weather.js
+++ weather.js
@@ -1,11 +1,18 @@
 #!/usr/bin/env node
+import { getWeather } from './services/api.service.js';
 import { getArgs } from './helpers/args.js';
 import { printHelp, printSuccess, printError } from './services/log.service.js';
-import { saveKeyValue } from './services/storage.service.js';
+import { saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js';
+
+
 
 const saveToken = async (token) => {
+    if (!token.length) {
+        printError('Не передан token');
+        return;
+    }
     try {
-        await saveKeyValue('token', token);
+        await saveKeyValue(TOKEN_DICTIONARY.token, token);
         printSuccess('Токен сохранён');
     } catch (e) {
         printError(e.message);
@@ -14,7 +21,6 @@ const saveToken = async (token) => {
 
 const initCLI = () => {
     const args = getArgs(process.argv)
-    console.log(args);
     if (args.h) {
         printHelp();
     }
@@ -24,6 +30,8 @@ const initCLI = () => {
     if (args.t) {
         return saveToken(args.t);
     }
+    const city = Object.values(args);
+    const data = getWeather(city[0]).then(res => { console.log(res); }); // 'krasnoyarsk'
     // Вывести погоду
 };
 
